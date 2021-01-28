52590a0e00bc6c11ee1d213438506ad76b4183c8
Ryan Woodbury <rywood89@gmail.com>
2021-01-27 21:50:52 -0500

    committing compiled r to html files

---
+++ test_markdown_github.R
@@ -0,0 +1,17 @@
+# ---
+# title: "Test Markdown"
+# author: "Ryan Woodbury"
+# date: "1/27/2021"
+# output: 
+#   html_document:
+#     keep_md: true
+# ---
+
+
+#```{r}
+x <- rnorm(100)
+y <- runif(100)
+
+plot(x,y)
+#```
+

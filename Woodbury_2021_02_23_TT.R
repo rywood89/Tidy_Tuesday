7e0bc0a5743f4bd68d3b5127a4f56fb1222e4d89
Ryan Woodbury <rywood89@gmail.com>
2021-02-24 13:49:31 -0500

    path graph

--- Woodbury_2021_02_23_TT.R
+++ Woodbury_2021_02_23_TT.R
@@ -37,6 +37,8 @@ earn %>% count(sex) # this looks OK, but some cleaning of the "Both Sexes"
 earn %>% count(race) # again, OK, but can filter out "All Races"
 earn %>% count(ethnic_origin) # `All Origins` or `Hispanic or Latino`
 earn %>% count(age) # Quite messy
+hist(earn$n_persons)
+hist(earn$median_weekly_earn)
 
 View(earn_filter <- earn %>% 
   filter(sex == "Both Sexes",
@@ -59,3 +61,22 @@ ggplot(earn_filter_long, aes(x = year_quarter, y = earn_ratio)) +
   scale_y_continuous(name = "Earnings Ratio", limits = c(0,1), labels = function(y) scales::percent(y)) +
   labs(title = "Ratio of earnings of Black and White employees",
        caption = "By: Ryan Woodbury | Tidy Tuesday R4DS community | Data from BLS.gov")
+
+
+earn_NoAll <- earn %>% 
+  filter(sex != "Both Sexes", 
+         race != "All Races",
+         str_detect(age, "and over"),
+         ethnic_origin == "All Origins") %>% 
+  mutate(race = recode(race, "Black or African American" = "Black"),
+         age = str_extract(age, "\\d+"),
+         date = lubridate::yq(paste(year, quarter,sep="-")))
+
+ggplot(earn_NoAll, aes(date, n_persons, color = race, shape = age)) +
+  geom_point() +
+  geom_path(aes(linetype = factor(sex)))
+
+
+ggplot(filter(earn_NoAll, race == "All Races" & age == "16"), aes(date, n_persons, color = age, shape = sex)) +
+  geom_point() +
+  geom_path()
